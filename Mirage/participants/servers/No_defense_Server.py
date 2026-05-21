e7eec93173fe4fcaf93fdea43041d7306481c3d1
Fatima <76473050+fabacha@users.noreply.github.com>
2026-05-21 15:32:25 +0100

    Optimize weight accumulation and model updates

--- Mirage/participants/servers/No_defense_Server.py
+++ Mirage/participants/servers/No_defense_Server.py
@@ -18,22 +18,21 @@ class No_defense_Server(BasicServer):
         # 查看No_defense_Server的所有参数
 
     def broadcast_upload(self, iteration, benign_client, malicious_client, **kwargs):
-
         logger.info(f"Training on global iteration {iteration} ")
-
+    
         selected_clients_list, malicious_clients_list = self.select_clients(iteration)
-        ''' 记录当前的训练中，有多少个恶意客户端'''
         current_no_of_adversaries = 0
         for client_id in selected_clients_list:
             if client_id in malicious_clients_list:
                 current_no_of_adversaries += 1
-
-
-        weight_accumulator = self.create_weight_accumulator()  # 初始化权重累加器, dict类型
+    
+        weight_accumulator = self.create_weight_accumulator()
         weight_accumulator_by_client = []
         update_norm_list = []
         global_model_copy = self.create_global_model_copy()
-        global_model = copy.deepcopy(self.global_model)
+        # OPTIMIZATION: Store state_dict reference instead of deepcopy
+        global_model_state = self.global_model.state_dict()
+        
         aggregated_model_id = [1] * self.params["no_of_participants_per_iteration"]
         for client_id in tqdm(selected_clients_list):
             if client_id in malicious_clients_list:
@@ -41,23 +40,25 @@ class No_defense_Server(BasicServer):
             else:
                 client = benign_client
             client_train_data = self.train_dataloader[client_id]
-
+    
             local_model = copy.deepcopy(self.global_model)
-
+    
             for name, params in local_model.named_parameters():
                 params.requires_grad = True
-
+    
             local_model.train()
             updated_model = client.local_train(iteration, local_model, client_train_data, client_id, test_loader=self.test_dataloader)
-            update_norm = model_dist_norm_var(updated_model, global_model_copy)  # 计算更新距离全局模型的二范数
-
+            update_norm = model_dist_norm_var(updated_model, global_model_copy)
+    
             update_norm_list.append(round(update_norm.item(), 6))
-
-            weight_accumulator, single_wa = update_weight_accumulator(updated_model, copy.deepcopy(self.global_model),
+    
+            # OPTIMIZATION: Pass state_dict reference by reference instead of deepcopy
+            weight_accumulator, single_wa = update_weight_accumulator(updated_model, global_model_state,
                                                                       weight_accumulator)
             weight_accumulator_by_client.append(single_wa)
             del local_model
-
-        for client_ind,client_id in enumerate(selected_clients_list):
+    
+        for client_ind, client_id in enumerate(selected_clients_list):
             logger.info(f"Client {client_id} update norm: {update_norm_list[client_ind]}")
         return weight_accumulator, weight_accumulator_by_client, aggregated_model_id
+    
